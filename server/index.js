6837899ae965e091d8c54a6ba1ffd1640a1a56b4
Mohamed Fariz Nazeer <faariz@gmail.com>
2019-02-10 19:31:49 +0800

    Fixes error in server SQL syntax

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ app.post('/values', async (req, res) => {
 
     redisCLient.hset('values', index, 'Nothings yet!');
     redisPublisher.publish('insert', index);
-    pgClient.query('INSERT INTO values(number) VALUES($1)' [index]);
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
     res.send({ working: true });
 });
