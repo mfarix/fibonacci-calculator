0997754bfec7ed759040fc6370e0a38cac4d17b2
Mohamed Fariz Nazeer <faariz@gmail.com>
2019-02-10 13:48:14 +0800

    Adds values APIs for calculator

--- server/index.js
+++ server/index.js
@@ -33,3 +33,38 @@ const redisCLient = redis.createClient({
 });
 
 const redisPublisher = redisCLient.duplicate();
+
+// Express Route Handlers
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * FROM values');
+
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    redisCLient.hgetall('values', (err, values) => {
+        res.send(values);
+    })
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+
+    if (parseInt(index) > 40) {
+        return res.status(422).send('Index too high!');
+    }
+
+    redisCLient.hset('values', index, 'Nothings yet!');
+    redisPublisher.publish('insert', index);
+    pgClient.query('INSERT INTO values(number) VALUES($1)' [index]);
+
+    res.send({ working: true });
+});
+
+app.listen(5000, err => {
+    console.log('Listening on port 5000');
+});
