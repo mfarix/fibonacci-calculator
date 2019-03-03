f51d0a7a205efffdab769057c2149c34bb611eb5
Mohamed Fariz Nazeer <faariz@gmail.com>
2019-03-04 01:33:25 +0800

    Fixes error on seen index

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -37,7 +37,7 @@ class Fib extends Component {
 
     renderSeenIndexes() {
         if (!this.state.seenIndexes || !this.state.seenIndexes.length) {
-            return this.state = { seenIndexes: [] };
+            this.state = { seenIndexes: [] };
         }
         return this.state.seenIndexes.map(({ number }) => number).join(', ');
     }
